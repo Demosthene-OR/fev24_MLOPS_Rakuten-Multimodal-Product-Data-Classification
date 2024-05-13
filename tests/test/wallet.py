3ca54379d407817faaa335e815c2c52b51c90086
fabdoulaye <73080697+fabdoulaye@users.noreply.github.com>
2024-05-14 00:49:21 +0200

    image_train from main
    image_train from main

---
+++ tests/test/wallet.py
@@ -0,0 +1,15 @@
+class Wallet(object):
+
+    def __init__(self, initial_amount=0):
+        self.balance = initial_amount
+
+    def spend_cash(self, amount):
+        if self.balance < amount:
+            raise InsufficientAmount('Not enough available to spend {}'.format(amount))
+        self.balance -= amount
+
+    def add_cash(self, amount):
+        self.balance += amount
+
+class InsufficientAmount(Exception):
+    pass
---
+++ tests/test_example.py
@@ -0,0 +1,29 @@
+import os
+import pytest
+import requests
+
+# Récupération des tokens d'accès depuis les variables d'environnement
+ACCESS_TOKEN_AUTH_0 = os.environ.get('ACCESS_TOKEN_AUTH_0')
+ACCESS_TOKEN_AUTH_1 = os.environ.get('ACCESS_TOKEN_AUTH_1')
+ACCESS_TOKEN_AUTH_2 = os.environ.get('ACCESS_TOKEN_AUTH_2')
+
+def test_calc_addition():
+    # Fonction test du résultat de 2+4
+    output = 2 + 4
+    assert output == 6
+    
+def test_access_authorization_0():
+    # Vérification de l'accès sécurisé en utilisant le token d'accès
+    # Authorization niveau 0 = Pas d'acces au Train du modèle ni à la prediction
+    auth_response = requests.get("http://localhost:8001/secured", headers={"Authorization": f"Bearer {ACCESS_TOKEN_AUTH_0}"})
+    assert auth_response.status_code == 200
+
+def test_access_authorization_1():
+    # Authorization niveau 1 = Accès seulement à la prediction, mais pas d'acces au Train du modèle
+    auth_response = requests.get("http://localhost:8001/secured", headers={"Authorization": f"Bearer {ACCESS_TOKEN_AUTH_1}"})
+    assert auth_response.status_code == 200
+
+def test_access_authorization_2():
+    # Authorization niveau 1 = Accès à la prediction et au Train du modèle
+    auth_response = requests.get("http://localhost:8001/secured", headers={"Authorization": f"Bearer {ACCESS_TOKEN_AUTH_2}"})
+    assert auth_response.status_code == 200
